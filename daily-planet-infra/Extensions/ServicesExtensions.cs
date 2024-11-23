1b1135d8eb9bf409604ff3f72d80d567ea5c199e
Arthur Vaz <arthur.vaz95@outlook.com>
2024-11-23 19:22:56 -0300

    fixes

--- daily-planet-infra/Extensions/ServicesExtensions.cs
+++ daily-planet-infra/Extensions/ServicesExtensions.cs
@@ -2,11 +2,6 @@
 using daily_planet_domain.Interface.Services;
 using daily_planet_infra.Context;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace daily_planet_infra.Extensions
 {
